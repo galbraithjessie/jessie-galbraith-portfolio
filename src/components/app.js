0ebe17dd64976faddde611d5c12769136b6bdd5a
Jessie Galbraith <galbraithjessie@gmail.com>
2020-02-20 11:27:52 -0700

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Jessie Galbraiths Portfolio</h1>
+      </div>
+    );
+  }
+}
