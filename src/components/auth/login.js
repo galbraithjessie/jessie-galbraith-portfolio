6ecbe0dbe81e9dcb6f5e4902656da11d72c6241f
Jessie Galbraith <galbraithjessie@gmail.com>
2020-03-12 12:52:11 -0600

    Adjusting Routes

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -17,7 +17,7 @@ export default class Login extends Component {
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value, 
+      [event.target.name]: event.target.value,
       errorText: ""
     });
   }
@@ -35,18 +35,20 @@ export default class Login extends Component {
         { withCredentials: true }
       )
       .then(response => {
-        if (response.data.status === 'created') {
-            console.log('You can come in..');
-            
+        if (response.data.status === "created") {
+          this.props.handleSuccessfulAuth();
         } else {
-            this.setState({
-                errorText: "Wrong email or password"
-            });
-        }
-      }).catch(error => {
           this.setState({
-              errorText: "An error occured"
+            errorText: "Wrong email or password"
           });
+          this.props.handleUnsuccessfulAuth();
+        }
+      })
+      .catch(error => {
+        this.setState({
+          errorText: "An error occurred"
+        });
+        this.props.handleUnsuccessfulAuth();
       });
 
     event.preventDefault();
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,47 +1,48 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class NavigationContainer extends Component {
-    constructor() {
-        super();
-    }
-
-
-    render() {
+const NavigationComponent = (props) => {
+    const dynamicLink = (route, linkText) => {
         return (
-            <div className="nav-wrapper">
-                <div className="left-side">
-                    <div className="nav-link-wrapper">
-                        <NavLink exact to="/" activeClassName="nav-link-active">
-                            Home
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/about-me" activeClassName="nav-link-active">
-                            About
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/contact" activeClassName="nav-link-active">
-                            Contact
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/blog" activeClassName="nav-link-active">
-                            Blog
-                        </NavLink>
-                    </div>
-
+            <div className="nav-link-wrapper">
+                <NavLink to="/blog" activeClassName="nav-link-active">
+                    Blog
+                </NavLink>
+            </div>
+        );
+    };
+    
+    return (
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink exact to="/" activeClassName="nav-link-active">
+                        Home
+                    </NavLink>
                 </div>
 
                 <div className="nav-link-wrapper">
-                    <div className="right-side">Jessie Galbraith</div>
+                    <NavLink to="/about-me" activeClassName="nav-link-active">
+                        About
+                    </NavLink>
                 </div>
 
+                <div className="nav-link-wrapper">
+                    <NavLink to="/contact" activeClassName="nav-link-active">
+                        Contact
+                    </NavLink>
+                </div>
+
+                {props.loggedInStatus === "LOGGED_IN" ?  dynamicLink("/blog", "Blog") : null}
+
             </div>
-        );
-    }
+
+            <div className="nav-link-wrapper">
+                <div className="right-side">Jessie Galbraith</div>
+            </div>
+
+        </div>
+    );
 }
+
+export default NavigationComponent;
