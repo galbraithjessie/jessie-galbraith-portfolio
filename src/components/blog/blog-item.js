e9edb908bdd47a9270436088ba40f3d649f80984
Jessie Galbraith <galbraithjessie@gmail.com>
2020-03-22 13:02:38 -0600

    setting up the blog js

---
+++ src/components/blog/blog-item.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BlogItem = props => {
+    const {
+        id,
+        title,
+        content,
+        blog_status,
+        featured_image_url
+    } = props.blogItem;
+
+    return (
+        
+        <div>
+            <h1>{title}</h1>
+        
+            <div>
+                {content}
+            </div>
+        </div>
+    );
+}
+
+export default BlogItem;
