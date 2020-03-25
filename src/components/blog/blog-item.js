0903d606ca4914cf5c9d94987f8f782020a04c4d
Jessie Galbraith <galbraithjessie@gmail.com>
2020-03-24 18:33:05 -0600

    Working with the blog page and scss

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BlogItem = props => {
     const {
@@ -12,8 +13,10 @@ const BlogItem = props => {
     return (
         
         <div>
-            <h1>{title}</h1>
-        
+            <Link to={`/b/${id}`}>
+                <h1>{title}</h1>
+            </Link>
+            
             <div>
                 {content}
             </div>
